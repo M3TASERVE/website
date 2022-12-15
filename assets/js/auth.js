6559793353376af6d2adefd6ace08369c8d88ff2
nheulot <nicolas.heulot@irt-systemx.fr>
2022-12-15 15:20:42 +0100

    fixing login

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,9 +1,19 @@
 import Alpine from `alpinejs`
 
+(async () => {
+
 var supabase = createSupabase();
 
-var infos = supabase.auth.user();
-console.log(JSON.stringify(infos));
+const { data: { session } } = await supabase.auth.getSession();
+
+var user = null;
+if (session != null) {
+    const { data, error } = await supabase.auth.getUser();
+    if (!error) {
+        console.log(data.user);
+        user = data.user;
+    }
+}
 
 var showToast = (self, message) => {
     self.msg = message; 
@@ -11,22 +21,11 @@ var showToast = (self, message) => {
     toast.show();
 }
 
-var handleResponse = (self, response) => {
-    if (response.error) { 
-        console.log(response.error.message); 
-        showToast(self, response.error.message);
-    } else { 
-        console.log(response); window.location="/"; 
-    }
-}
-var handleError = (self, err) => {
-    console.log(err);
-    showToast(self, err.response.text);
-}
+var redirectToHome = () => { setTimeout(() => { window.location="/"; }, 1000); }
 
 Alpine.data('user', () => ({
-    identity: infos ? infos.identities[0].identity_data : { email: "", sub: "" },
-    logged: infos != null,
+    identity: user ? { email: user.email, sub: user.id } : { email: "", sub: "" },
+    logged: user != null,
     results: [],
     progression: {},
     submitted: {},
@@ -34,7 +33,8 @@ Alpine.data('user', () => ({
     msg: "",
     form: {
         email: { value: "", errorMessage: "" },
-        password: { value: "", errorMessage: "" },
+        password: { value: "", errorMessage: "" },        
+        passwordConfirm: { value: "", errorMessage: "" },
         validation: { successMessage: "", errorMessage: "" }
     },
     submission: {
@@ -49,58 +49,82 @@ Alpine.data('user', () => ({
         let flag = true;
         var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
         if (this.form.email.value.length == 0 || !this.form.email.value.match(validRegex)) { this.form.email.errorMessage = "Please enter a valid email."; flag = false; } else { this.form.email.errorMessage = ""; }
-        if (submitType != 'reset' && this.form.password.value.length < 10 ) { this.form.password.errorMessage = "Password length must be at least 10 characters."; flag = false; } else { this.form.password.errorMessage = ""; }
-        if (flag) { if (submitType == 'signin') this.signin(); else if (submitType == 'signup') this.signup(); else if (submitType == 'reset') this.reset(); }
+        if (submitType == 'signup' && this.form.password.value != this.form.passwordConfirm.value) { this.form.passwordConfirm.errorMessage = "Passwords are different."; flag = false; } else { this.form.passwordConfirm.errorMessage = ""; }
+        if (submitType != 'reset' && this.form.password.value.length < 10) { this.form.password.errorMessage = "Password length must be at least 10 characters."; flag = false; } else { this.form.password.errorMessage = ""; }
+        if (flag) { if (submitType == 'signin') this.signin(); else if (submitType == 'signup') this.signup(); else if (submitType == 'reset') this.reset(); else if (submitType == 'password') this.password(); }
     },
-    signin() {
+    isDone(x) {
+        return (x in this.progression);
+    },
+    async signin() {
         var self = this;    
         self.form.validation.errorMessage = "";
         self.form.validation.successMessage = "";
-        supabase.auth
-        .signIn({ email: self.form.email.value, password: self.form.password.value })
-        .then((response) => {
-            if (response.error) { 
-                console.log(response.error.message);
-                self.form.validation.errorMessage = "We were unable to verify your credentials";
-            } else { 
-                console.log(response);
-                self.form.validation.successMessage = "Login success !";
-                setTimeout(() => { window.location="/"; }, 2000);
-            }
-        })
-        .catch((err) => { handleError(this, err); });
+        const { data, error } = await supabase.auth.signInWithPassword({ email: self.form.email.value, password: self.form.password.value }, { redirectTo: window.location.origin });
+        if (error != null) {
+            console.log(error);
+            self.form.validation.errorMessage = "We were unable to verify your credentials";
+        } else {
+            console.log(data);
+            self.form.validation.successMessage = "Login success !";
+            redirectToHome();
+        }
     },
-    signup() {
+    async signup() {
         var self = this;
-        supabase.auth
-        .signUp({ email: self.form.email.value, password: self.form.password.value })
-        .then((response) => { handleResponse(this, response); })
-        .catch((err) => { handleError(this, err); });
+        const { data, error } = await supabase.auth.signUp({ email: self.form.email.value, password: self.form.password.value })
+        if (error != null) {
+            console.log(error);
+            showToast(self, error);
+        } else {
+            console.log(data);
+            self.form.validation.successMessage = "Email sent !";
+            redirectToHome();
+        }
     },
-    signout() {
-        supabase.auth.signOut()
-        .then((response) => { handleResponse(this, response); })
-        .catch((err) => { handleError(this, err); });
+    async signout() {
+        const { data, error } = await supabase.auth.signOut()
+        if (error != null) {
+            console.log(error);
+            showToast(self, error);
+        } else {
+            console.log(data);
+            redirectToHome();
+        }
     },
-    reset() {
+    async reset() {        
         var self = this;
-        supabase.auth.api
-        .resetPasswordForEmail(self.form.email.value)
-        .then((response) => { handleResponse(this, response); })
-        .catch((err) => { handleError(this, err); });
+        const { data, error } = await supabase.auth.resetPasswordForEmail(self.form.email.value, { redirectTo: window.location.origin+"/password" })
+        if (error != null) {
+            console.log(error);
+            showToast(self, error);
+        } else {
+            console.log(data);
+            self.form.validation.successMessage = "Email sent !";
+            redirectToHome();
+        }
     },
-    publish(id_lesson, data_type, content) {
+    async password() {
         var self = this;
-        supabase.from('results').insert({ number: id_lesson, type: data_type, content: content, created_by: self.identity.sub }).single()
-        .then((response) => {
-            if (response.error) { 
-                console.log(response.error.message); 
-                showToast(self, response.error.message);
-            } else { 
-                console.log(response);
-            }
-        })
-        .catch((err) => { handleError(this, err); });
+        const { data, error } = await supabase.auth.updateUser({ password: self.form.password.value });
+        if (error != null) {
+            console.log(error);
+            showToast(self, error);
+        } else {
+            console.log(data);
+            self.form.validation.successMessage = "Password changed !";
+            redirectToHome();
+        }
+    },
+    async publish(id_lesson, data_type, content) {
+        var self = this;
+        var response = await supabase.from('results').insert({ number: id_lesson, type: data_type, content: content, created_by: self.identity.sub }).single()
+        if (response.error) { 
+            console.log(response.error.message); 
+            showToast(self, response.error.message);
+        } else { 
+            console.log(response);
+        }
     },
     async retrieve() {
         var self = this;
@@ -116,10 +140,9 @@ Alpine.data('user', () => ({
                 self.loaded = true;
             }
         }
-    },
-    isDone(x) {
-        return (x in this.progression);
     }
 }))
 
-Alpine.start()
+Alpine.start();
+
+})();
